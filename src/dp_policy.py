faf875113f3d2a71dfab46534d41da1322778cd0
Jason Zhu <jasonzy121@gmail.com>
2018-03-01 22:58:27 -0800

    Update dp

--- src/dp_policy.py
+++ src/dp_policy.py
@@ -13,10 +13,17 @@ parser = argparse.ArgumentParser(description='Dynamic Programming Algorithm')
 parser.add_argument('--file_msg', default= '../datasets/GOOG_2012-06-21_34200000_57600000_message_10.csv', help='Message File Path')
 parser.add_argument('--file_order', default= '../datasets/GOOG_2012-06-21_34200000_57600000_orderbook_10.csv', help= 'Order File Path')
 parser.add_argument('--base_point', default=100, help='Base Point', type=int)
-parser.add_argument('--order_direction', default=1, help='Buy 1, Sell -1', type=int)
-parser.add_argument('--spread_cutoff', default=1.0, help='Cutoff for low bid-ask spread/high spread', type=float)
-parser.add_argument('--start', default=34201, help='Start Time', type=float)
-parser.add_argument('--end', default=34300, help='End Time', type=float)
+parser.add_argument('--order_direction', default=-1, help='Buy 1, Sell -1', type=int)
+parser.add_argument('--spread_cutoff', default=10.0, help='Cutoff for low bid-ask spread/high spread', type=float)
+parser.add_argument('--train_start', default=34200, help='Train Start Time', type=float)
+parser.add_argument('--train_end', default=46800, help='Train End Time', type=float)
+parser.add_argument('--test_start', default=46800, help='Test End Time', type=float)
+parser.add_argument('--train_end', default=57600, help='Test End Time', type=float)
+parser.add_argument('--H', default=600, help='Horizon', type=float)
+parser.add_argument('--T', default=20, help='Horizon', type=int)
+parser.add_argument('--V', default=100, help='Horizon', type=int)
+parser.add_argument('--I', default=10, help='Horizon', type=int)
+parser.add_argument('--L', default=10, help='Horizon', type=int)
 args = parser.parse_args()
 
 def Calculate_Q(V, H, T, I, L):
@@ -29,7 +36,7 @@ def Calculate_Q(V, H, T, I, L):
     """
     Q = np.zeros((T + 2, I, states_len[0], states_len[1], L))
     Q_counter = np.zeros((T + 2, I, states_len[0], states_len[1], L))
-    for t in np.arange(T, 0, -1):
+    for t in np.arange(T, -1, -1):
         time = H*(t/T)
         next_time = time + H/T
         """
@@ -45,8 +52,13 @@ def Calculate_Q(V, H, T, I, L):
             for i in range(I):
                 for a in range(L):
                     a_price = prices[a]
-                    episode_next_state = get_state(episode[1])
-                    episode_next_i, im_reward = simulate(episode[0], (i+1)*int(V/I) , a_price, real_time[0], real_time[1])
+                    if t == T:
+                        episode_next_state = get_state(episode[0])
+                        episode_next_i, im_reward = simulate(episode[0], int((i+1)*V/I) , a_price, real_time[0], real_time[0])
+                    else:
+                        episode_next_state = get_state(episode[1])
+                        episode_next_i, im_reward = simulate(episode[0], int((i+1)*V/I) , a_price, real_time[0], real_time[1])
+
                     episode_next_i = int(episode_next_i/V*I)-1 # Have to change new order_size into inventory units.
                     max_Q = np.amax(Q[t+1, episode_next_i, episode_next_state[0], episode_next_state[1], :])
                     n = Q_counter[t, i, episode_states[0], episode_states[1], a]
@@ -59,17 +71,21 @@ def Optimal_strategy(Q):
     """
     return argmax of each Q along the last axis (action)
     """
-    return np.argmax(Q, axis=len(Q.shape)-1)
+    if args.order_direction = -1:
+        return np.argmax(Q, axis=len(Q.shape)-1)
+    elif args.order_direction = 1:
+        return np.argmin(Q, axis=len(Q.shape)-1)
 
 
 def load_episodes(time, next_time, H, V):
     lob1_data, time_1 = read_order_book(time, H)
-    lob1 = [Limit_Order_book(**lob_data, own_amount_to_trade=V,
+    print (lob1_data[0])
+    lob1 = [Limit_Order_book(**lob_data, own_amount_to_trade = 0, 
                     own_init_price=-args.order_direction*Limit_Order_book._DUMMY_VARIABLE,
                     own_trade_type=args.order_direction) for lob_data in lob1_data]
 
     lob2_data, time_2 = read_order_book(next_time, H)
-    lob2 = [Limit_Order_book(**lob_data, own_amount_to_trade=V,
+    lob2 = [Limit_Order_book(**lob_data, own_amount_to_trade = 0,
                     own_init_price=-args.order_direction*Limit_Order_book._DUMMY_VARIABLE,
                     own_trade_type=args.order_direction) for lob_data in lob2_data]
     return list(zip(lob1, lob2)), list(zip(time_1, time_2))
@@ -79,15 +95,15 @@ def read_order_book(time, H):
     """
     read the initial limit order book states from the file
     """
-    output= []
-    time_output= []
-    order= Order_Queue(args.file_order)
-    real_time= args.start+time
-    while real_time<args.end:
-        mq= Message_Queue(args.file_msg)
+    output = []
+    time_output = []
+    order = Order_Queue(args.file_order)
+    real_time = args.train_start + time
+    while real_time < args.train_end:
+        mq = Message_Queue(args.file_msg)
         output.append(order.create_orderbook_time(real_time, mq))
         time_output.append(real_time)
-        real_time= real_time+H
+        real_time= real_time + H
     return output, time_output
 
 
@@ -115,20 +131,21 @@ def simulate(lob, amount, a_price, time, next_time):
     (revenue from the executed orders)
     """
     mq = Message_Queue(args.file_msg)
-    mq.pop_to_next_time(time)
+    for idx, msg in mq.pop_to_next_time(time):
+        pass
 
     lob_copy = copy.deepcopy(lob)
-    lob_copy.update_own_order(a_price,amount)
-    mq_copy = copy.deepcopy(mq)
+    lob_copy.update_own_order(a_price, amount)
 
-    for idx, message in mq_copy.pop_to_next_time(next_time):
+    for idx, message in mq.pop_to_next_time(next_time):
         lob_copy.process(**message)
         if lob_copy.own_amount_to_trade == 0:
             break
 
     return [lob_copy.own_amount_to_trade, lob_copy.own_reward]
 
-print (np.min(Calculate_Q(12, 10, 4, 2, 5)))
-#V, H, T, I, L
+path_target = '../data/Q_dp.npy'
+np.save(path_target, Calculate_Q(args.V, args.H, args.T, args.I, args.L))
+
 
 
