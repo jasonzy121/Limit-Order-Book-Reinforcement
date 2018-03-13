a72c0ed72e02fd6411854d314f323e9de069b73e
Luyang Chen <LouisChen1992@hotmail.com>
2018-03-13 15:48:09 -0700

    add cnn model

--- src/Neural_Net.py
+++ src/Neural_Net.py
@@ -10,14 +10,15 @@ from dqn_model import DQN
 
 class Neural_DQN(DQN):
 	def get_q_values_op(self, state, scope, reuse= False):
-		hidden_size= self._config.hidden_size
 		num_actions = self._config.L + 1 # 1 for market order
 		state_book, state_it = state
+
 		with tf.variable_scope(scope, reuse=reuse):
-			state_book_flattened = layers.flatten(state_book)
-			state_out = tf.concat([state_book_flattened, state_it], axis=1)
-			out_1 = layers.fully_connected(state_out, hidden_size)
-			out= layers.fully_connected(out_1, num_actions)
+			conv_1 = layers.conv2d(inputs=state_book, num_outputs=4, kernel_size=[3,3], stride=[1,1], activation_fn=tf.nn.relu, padding='same')
+			conv_2 = layers.conv2d(inputs=conv_1, num_outputs=4, kernel_size=[3,3], stride=[1,1], activation_fn=tf.nn.relu, padding='same')
+			conv_2_flattened = layers.flatten(inputs=conv_2)
+			state_out = tf.concat([conv_2_flattened, state_it], axis=1)
+			out = layers.fully_connected(state_out, num_actions, activation_fn=None)
 		return out
 
 if __name__ == '__main__':
